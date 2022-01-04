5914380ea69795f1704925aa06905e5a95878cef
Paul Hewson <texhewson@gmail.com>
2022-01-04 16:04:59 +0000

    remove vms

--- splunk/tests/simulate_data_load_and_query.sh
+++
@@ -1,8 +0,0 @@
-TOKEN="$1"
-QUERY=$(cat test.spl)
-FULLQUERY="search=search $QUERY"
-
-curl -k -H "Authorization: Splunk $TOKEN" --request DELETE https://localhost:8089/services/data/indexes/testing > errors.xml
-curl -k -H "Authorization: Splunk $TOKEN" https://localhost:8089/servicesNS/phewson@wifispark.com/search/data/indexes -d name=testing >> errors.xml
-curl -k -H "Authorization: Splunk $TOKEN" https://192.168.31.31:8089/services/data/inputs/oneshot  --data-urlencode name='/home/vagrant/simulations.json' -d host=testing -d sourcetype=testing_json -d index=testing >> errors.xml
-curl -k -H "Authorization: Splunk $TOKEN" https://localhost:8089/services/search/jobs/export --data-urlencode "$FULLQUERY" -d output_mode=csv > out.csv
