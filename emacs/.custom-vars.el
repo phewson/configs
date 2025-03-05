ded64cc5018b2b7f2945d974bc9a308296ecc150
Paul Hewson <paul.hewson@overleaf.com>
2025-03-05 16:08:08 +0000

    Nearly committed to using dot_emacs.org
    Final set of testing with the idea of using dot_emacs.org and tangling
    to .emacs

--- emacs/.custom-vars.el
+++ emacs/.custom-vars.el
@@ -4,7 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-drill ox-reveal tree-sitter-langs tree-sitter corfu-doc xenops writeroom-mode which-key wgrep vundo vertico tabspaces stan-mode rainbow-delimiters pulsar projectile powerline poly-R pdf-tools org-ref org-bullets marginalia magit lsp-ui lsp-latex languagetool json-mode ivy-bibtex hackernews gscholar-bibtex gif-screencast general flycheck ess elfeed docker-compose-mode docker devdocs corfu company-auctex citar-org-roam cdlatex browse-kill-ring auto-package-update all-the-icons-dired all-the-icons-completion academic-phrases)))
+   '(academic-phrases all-the-icons-completion all-the-icons-dired
+		      auto-package-update browse-kill-ring cdlatex
+		      citar-org-roam company-auctex corfu corfu-doc
+		      devdocs docker docker-compose-mode eglot-jl
+		      elfeed ess flycheck general gif-screencast
+		      gscholar-bibtex hackernews ivy-bibtex json-mode
+		      languagetool lsp-latex lsp-ui magit marginalia
+		      org-bullets org-drill org-ref ox-reveal
+		      pdf-tools poly-R powerline projectile pulsar
+		      rainbow-delimiters stan-mode tabspaces
+		      tree-sitter tree-sitter-langs vertico vundo
+		      wgrep which-key writeroom-mode xenops)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
